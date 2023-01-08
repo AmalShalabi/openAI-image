0e4b51212efb0bce86521a4b777887f0b8bd6f46
unknown <mouni.shalaby@gmail.com>
2023-01-08 12:25:05 +0200

    open AI-frontend

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const path = require("path");
 const express = require("express");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
+var cors = require("cors");
+
 const port = process.env.PORT || 5000;
 
 const app = express();
-//Enable body parser
+app.use(cors());
+
+// Enable body parser
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/openai", require("./routes/openaiRoutes"));
 
 
-app.use('/openai',require('./routes/openaiRoutes'))
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+app.listen(port, () => console.log(`Server started on port ${port}`));
